491ec77cbcb756f4b5c7c340d3b2b6390843cc1a
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-10-09 13:10:35 -0400

    Configured UI TimeCircuitsVC

--- OutaTime/OutaTime/TimeCircuitsViewController.swift
+++ OutaTime/OutaTime/TimeCircuitsViewController.swift
@@ -10,28 +10,52 @@ import UIKit
 
 class TimeCircuitsViewController: UIViewController {
 
+    // MARK: - Outlets
     @IBOutlet weak var destinationTimeLabel: UILabel!
-    
     @IBOutlet weak var presentTimeLabel: UILabel!
-    
     @IBOutlet weak var lastTimeDepartedLabel: UILabel!
-    
     @IBOutlet weak var speedLabel: UILabel!
    
+    // MARK: - Properties
+    var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM dd   yyyy"
+        return formatter
+    }()
+    
+    var currentSpeed = 0
+    let today = Date()
+        
+    
+    // MARK: - Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateViews()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func setDestinationTimeButton(_ sender: UIButton) {
-    }
     
     @IBAction func travelBackButton(_ sender: UIButton) {
     }
     
+    private func updateViews() {
+        let todayString = string(from: today)
+        
+        presentTimeLabel.text = todayString
+        speedLabel.text = "\(currentSpeed) MPH"
+        lastTimeDepartedLabel.text = "---   --   ----"
+        
+        
+    }
+    
+    private func string(from date: Date) -> String {
+        return dateFormatter.string(for: date)!
+
+    }
+ 
+    
     /*
-    // MARK: - Navigation
+    // MARK: Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
