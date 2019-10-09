4873a5e11d584dba390a899b281a86f10c09c7d6
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-10-08 23:46:20 -0400

    Tweaked UI in TimePicker VC

--- OutaTime/OutaTime/TimeCircuitsViewController.swift
+++ OutaTime/OutaTime/TimeCircuitsViewController.swift
@@ -10,13 +10,26 @@ import UIKit
 
 class TimeCircuitsViewController: UIViewController {
 
+    @IBOutlet weak var destinationTimeLabel: UILabel!
+    
+    @IBOutlet weak var presentTimeLabel: UILabel!
+    
+    @IBOutlet weak var lastTimeDepartedLabel: UILabel!
+    
+    @IBOutlet weak var speedLabel: UILabel!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func setDestinationTimeButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func travelBackButton(_ sender: UIButton) {
+    }
+    
     /*
     // MARK: - Navigation
 
